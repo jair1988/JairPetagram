b608958c15519ad7f38e130055d3bde005e203c0
jair1988 <59373763+jair1988@users.noreply.github.com>
2020-08-11 18:43:41 -0500

    Pagina de configuracion

--- app/src/main/java/com/jair/petagram/restApi/ConstantesRestApi.java
+++ app/src/main/java/com/jair/petagram/restApi/ConstantesRestApi.java
@@ -3,7 +3,7 @@ package com.jair.petagram.restApi;
 public final class ConstantesRestApi {
     public static final String ROOT_URL = "https://graph.instagram.com/";
     public static final String KEY_ACCESS_TOKEN = "&access_token=";
-    public static final String ACCESS_TOKEN = "IGQVJYR0Jfa3J4amEyQXYtbm9oYVl6dlc5ZATlUN1pLekdXcVNCMlgwbzlIc0ZAFYk1kdlVBN3NTSV94N0xmLXVSRVNNNFh5bXdjVGU0XzUtWXRYOTI1VDhGZAktrc1JRRUhTNy1ENWRB";
+    public static final String ACCESS_TOKEN = "";
     public static final String KEY_GET_MEDIA_USER = "me/media?fields=media_url,username";
     public static final String URL_GET_MEDIA_USER = KEY_GET_MEDIA_USER + KEY_ACCESS_TOKEN + ACCESS_TOKEN;
 }
