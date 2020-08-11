006902d2ca1de7485ed24bee9510180b4d201dd6
jair1988 <59373763+jair1988@users.noreply.github.com>
2020-08-11 18:25:33 -0500

    Pagina de configuracion

--- app/src/main/java/com/jair/petagram/SettingActivity.java
+++ app/src/main/java/com/jair/petagram/SettingActivity.java
@@ -3,6 +3,8 @@ package com.jair.petagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+    }
+
+    public void save(View v){
+        Toast.makeText(this, "cuenta instagram", Toast.LENGTH_SHORT).show();
     }
 }
